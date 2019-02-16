baad3e22578562e31994279e8a0a87568821b0a4
BombomDeer <seanwon77@gmail.com>
2019-02-16 13:30:39 +0900

    whatever

---
+++ 20180216 Play Character/Assets/01.Script/Character.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IdleState();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void IdleState()
+    {
+        string triggerName = "idle1";
+        int randValue = Random.Range(1, 6);
+        triggerName = "idle" + randValue;
+        _animator.SetTrigger(triggerName);
+    }
+}
