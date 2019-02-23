0acd9028891a5198630f813162056c773ef686ea
BombomDeer <seanwon77@gmail.com>
2019-02-23 13:29:02 +0900

    Implemented Animation and a simple state machine

--- 20180216 Play Character/Assets/01.Script/Character.cs
+++ 20180216 Play Character/Assets/01.Script/Character.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    [SerializeField] Animator _animator;
+    [SerializeField] AnimationController _animationController;
     // Start is called before the first frame update
     void Start()
     {
-        IdleState();
+        //IdleState();
+        //_animationController.AddEndEvent(()=>
+        //{
+        //    Debug.Log("Animation Test");
+        //});
+        ChangeState(eState.IDLE);
     }
 
     // Update is called once per frame
@@ -17,11 +22,42 @@ public class Character : MonoBehaviour
         
     }
 
+    enum eState
+    {
+        IDLE, WAIT, KICK,
+    }
+
+    void ChangeState(eState state)
+    {
+        switch(state)
+        {
+            case eState.IDLE:
+                IdleState();
+                break;
+            case eState.WAIT:
+                WaitState();
+                break;
+            case eState.KICK:
+                KickState();
+                break;
+        }
+    }
+
+
     void IdleState()
     {
-        string triggerName = "idle1";
-        int randValue = Random.Range(1, 6);
-        triggerName = "idle" + randValue;
-        _animator.SetTrigger(triggerName);
+        _animationController.Play("idle1", () => { WaitState(); });
+
     }
+
+    void WaitState()
+    {
+        _animationController.Play("idle2", () => { KickState(); });
+    }
+
+    void KickState()
+    {
+        _animationController.Play("idle5", () => { IdleState(); });
+    }
+
 }
