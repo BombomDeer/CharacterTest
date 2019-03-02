c8780c2c4ad14191766122a4b153842a2cd38131
BombomDeer <seanwon77@gmail.com>
2019-03-02 13:22:59 +0900

    implemented OOP

--- 20180216 Play Character/Assets/01.Script/Character.cs
+++ 20180216 Play Character/Assets/01.Script/Character.cs
@@ -8,56 +8,103 @@ public class Character : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //IdleState();
-        //_animationController.AddEndEvent(()=>
-        //{
-        //    Debug.Log("Animation Test");
-        //});
+        //IdleState _idleState = new IdleState();
+        //WaitState _waitState = new WaitState();
+        //KickState _kickState = new KickState();
+
+        _stateDic.Add(eState.IDLE, new IdleState());
+        _stateDic.Add(eState.WAIT, new WaitState());
+        _stateDic.Add(eState.KICK, new KickState());
+
+        for(int i = 0; i<_stateDic.Count;i++)
+        {
+            eState state = (eState)i;
+            _stateDic[state].SetCharacter(this);
+        }
+
+        //_idleState.SetCharacter(this);
+        //_waitState.SetCharacter(this);
+        //_kickState.SetCharacter(this);
+
         ChangeState(eState.IDLE);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateState();
     }
 
-    enum eState
+    public enum eState
     {
-        IDLE, WAIT, KICK,
+        IDLE,
+        WAIT,
+        KICK, 
     }
 
-    void ChangeState(eState state)
+    public void ChangeState(eState state)
     {
-        switch(state)
-        {
-            case eState.IDLE:
-                IdleState();
-                break;
-            case eState.WAIT:
-                WaitState();
-                break;
-            case eState.KICK:
-                KickState();
-                break;
-        }
+        _state = _stateDic[state];
+        _state.Start();
+        //switch (state)
+        //{
+        //    case eState.IDLE:
+        //        //IdleStart();
+        //        _state = _idleState;
+        //        break;
+        //    case eState.WAIT:
+        //        //WaitStart();
+        //        _state = _waitState;
+        //        break;
+        //    case eState.KICK:
+        //        _state = _kickState;
+        //        break;
+        //}
+        //_state.Start();
     }
 
-
-    void IdleState()
+    void UpdateState()
     {
-        _animationController.Play("idle1", () => { WaitState(); });
-
+        _state.Update();
     }
 
-    void WaitState()
+    Dictionary<eState, State> _stateDic = new Dictionary<eState, State>();
+
+    State _state = null;
+    
+    //void IdleStart()
+    //{
+    //    _idleState.Start();
+    //    //_animationController.Play("idle1", () => 
+    //    //{
+    //    //    ChangeState(eState.WAIT);
+    //    //    //WaitState();
+    //    //});
+
+    //}
+
+    //void WaitStart()
+    //{
+    //    _animationController.Play("idle2", () => 
+    //    {
+    //        ChangeState(eState.WAIT2);
+    //        //KickState();
+    //    });
+    //}
+    
+
+
+    //void KickStart()
+    //{
+    //    _animationController.Play("idle5", () => 
+    //    {
+    //        ChangeState(eState.IDLE);
+    //        //IdleState();
+    //    });
+    //}
+
+    public void PlayAnimation(string trigger, System.Action endCallback)
     {
-        _animationController.Play("idle2", () => { KickState(); });
+        _animationController.Play(trigger, endCallback);
     }
-
-    void KickState()
-    {
-        _animationController.Play("idle5", () => { IdleState(); });
-    }
-
 }
