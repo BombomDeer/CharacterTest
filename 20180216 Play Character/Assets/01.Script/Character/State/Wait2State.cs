03d9b423aab4fc65e53650f0b60e83114f22dcac
Bombom <seanwon77@gmail.com>
2019-03-23 13:26:32 +0900

    Added Chracter Generator and collision behaviour between characters

---
+++ 20180216 Play Character/Assets/01.Script/Character/State/Wait2State.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wait2State : State
+{
+	override public void Start()
+	{
+
+		string AnimationString;
+		Dictionary<int, string> CorespondingAnimaiton = new Dictionary<int, string>();
+		CorespondingAnimaiton.Add(2, "idle3");
+		CorespondingAnimaiton.Add(3, "idle4");
+		CorespondingAnimaiton.Add(4, "kick");
+
+		int i;
+
+		i = Random.Range(2, 4);
+
+		_character.PlayAnimation(CorespondingAnimaiton[i], () =>
+		{
+			_character.ChangeState(Character.eState.IDLE);
+		});
+	}
+}
---
+++ 20180216 Play Character/Assets/01.Script/CharacterGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterGenerator : MonoBehaviour
+{
+	[SerializeField] GameObject _characterPrefab;
+	[SerializeField] List<GameObject> _wayPointList;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		//Generate();
+		StartCoroutine(ExecGenerate());
+		//GameObject obj = GameObject.Instantiate<GameObject>(_characterPrefab);
+		//obj.transform.position = Vector3.zero;
+		//obj.transform.rotation = Quaternion.identity;
+		//obj.transform.localScale = Vector3.one;
+
+
+		//Character character = obj.GetComponent<Character>();
+		//character.SetWayPointList(_wayPointList);
+	}
+
+	IEnumerator ExecGenerate()
+	{
+		while(true)
+		{
+			Generate();
+			yield return new WaitForSeconds(5.0f);
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void Generate()
+	{
+		GameObject obj = GameObject.Instantiate<GameObject>(_characterPrefab);
+		obj.transform.position = Vector3.zero;
+		obj.transform.rotation = Quaternion.identity;
+		obj.transform.localScale = Vector3.one;
+
+
+		Character character = obj.GetComponent<Character>();
+		character.SetWayPointList(_wayPointList);
+	}
+}
